3edc08d5c1df12ff54cc59d7bb358aed56d375cd
Wakype <hilmim152bisnis@gmail.com>
2022-07-27 11:32:53 +0700

    ReactJS

---
+++ src/Component/module/tes.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Page from "../../page";
+
+function Tes() {
+  return (
+    <React.Fragment>
+      <h3>tes tes tes ini tes</h3>
+      <Page />
+    </React.Fragment>
+  );
+}
+
+export default Tes;
---
+++ src/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Page() {
+    return(
+        <React.Fragment>
+            <h2>Ini adalah page</h2>
+        </React.Fragment>
+    )
+}
+
+export default Page
