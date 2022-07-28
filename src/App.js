0fc470c6f3877fcf99e3a9b62bc9893dffa1e8ba
Wakype <hilmim152bisnis@gmail.com>
2022-07-28 14:38:46 +0700

    belajar props bagian 1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from "react"
-import Header from "./Component/header"
-import Tes from "./Component/module/tes"
-import {Button, Input} from "./Component/module/name"
+import React from "react";
+import "./styles/style.css";
+import Identitas from "./module/indentitas";
+import Nilai from "./module/nilai";
 
 function App() {
-  return(
+  let [data, setData] = React.useState([10, 20, 30, 40, 50]);
+
+  return (
     <React.Fragment>
-      <h1 style={{textAlign: 'center'}}>Latihan Export import</h1>
-      <Header/>
-      <Tes/>
-      <Input/>
-      <Button/>
+      <h1 className="header">Latihan Props</h1>
+      <section>
+        <Identitas nama={"Hilmi"} kelas={"XI RPL"} nilai={100} />
+        <Identitas nama={"Radhy"} kelas={"XI TKJ"} nilai={90} />
+        <Identitas nama={"Nabil"} kelas={"XI RPL"} nilai={100} />
+        <Identitas nama={"Harry"} kelas={"XI TKJ"} nilai={80} />
+        <Identitas />
+      </section>
+      <Nilai nama={"waky"} data={data} />
     </React.Fragment>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/module/nilai.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Nilai({ nama, data }) {
+  console.log("nama", nama);
+  console.table(data);
+  return (
+    <React.Fragment>
+      <div>
+        <h1>Daftar Nilai</h1>
+        <h2>{nama}</h2>
+        <ol>
+          {/* <li>{data[0]}</li>
+          <li>{data[1]}</li>
+          <li>{data[2]}</li>
+          <li>{data[3]}</li>
+          <li>{data[4]}</li> */}
+          {data?.map((item, index) => {
+            return (
+              <li>
+                Nilai ujian ke-{index + 1} adalah {item}
+              </li>
+            );
+          })}
+        </ol>
+      </div>
+    </React.Fragment>
+  );
+}
