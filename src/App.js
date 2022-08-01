a98e818988347f903b50cf8f6f06a5895b143ffb
Wakype <hilmim152bisnis@gmail.com>
2022-08-01 11:12:40 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from "react"
-import Header from "./Component/header"
-import Tes from "./Component/module/tes"
-import {Button, Input} from "./Component/module/name"
+
 
 function App() {
   return(
     <React.Fragment>
       <h1 style={{textAlign: 'center'}}>Latihan Export import</h1>
-      <Header/>
-      <Tes/>
-      <Input/>
-      <Button/>
     </React.Fragment>
   )
 }
