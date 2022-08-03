faf80256021a2c2c4070378d1c6aceb910a39866
Wakype <hilmim152bisnis@gmail.com>
2022-08-03 09:57:29 +0700

    Latihan1_Props

--- src/module/produk.jsx
+++ src/module/produk.jsx
@@ -7,13 +7,23 @@ export default function DataProduk({ produk }) {
         return (
           <React.Fragment>
             <section>
-              <h3>Jenis: {item.jenis}</h3>
-              <h3>Produk: {item.produk}</h3>
-              <h1>Tipe</h1>
-              <p>{item.brand[0].nama}</p>
-              <p>{item.brand[0].harga}</p>
-              <p>{item.brand[1].nama}</p>
-              <p>{item.brand[1].harga}</p>
+              <div>
+                <div className="jenis">
+                  <h3>Jenis: {item.jenis}</h3>
+                  <h3>Produk: {item.produk}</h3>
+                </div>
+                <h1 className="tipe">Tipe</h1>
+                <div className="brand">
+                  <div className="brand1">
+                    <p>{item.brand[0].nama}</p>
+                    <p>{item.brand[0].harga}</p>
+                  </div>
+                  <div className="brand2">
+                    <p>{item.brand[1].nama}</p>
+                    <p>{item.brand[1].harga}</p>
+                  </div>
+                </div>
+              </div>
             </section>
           </React.Fragment>
         );
