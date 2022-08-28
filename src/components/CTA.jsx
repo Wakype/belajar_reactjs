6bd87be2c20a6a27f1b0dd3a2b9cd23c11ea6b1a
Wakype <hilmim152bisnis@gmail.com>
2022-08-28 10:50:57 +0700

    JavaScriptMastery 1

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import styles from '../style'
+import Button from './Button'
+
 
 const CTA = () => {
   return (
-    <div>
-      CTA
-    </div>
+    <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
+      <div className="flex-1 flex flex-col">
+        <h2 className={`${styles.heading2} `}>Let's tru our service now!</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam culpa quos dolores repellendus facilis vitae at hic.
+        </p>
+      </div>
+
+      <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
+        <Button/>
+      </div>
+    </section>
   )
 }
 
